626f262714a115f923d1267b89c3c5509d81d5ae
Admin <Admin@DESKTOP-15H4UB6>
2023-04-25 16:18:35 +0300

    clear lastStatuses in RefreshDevices

--- SwitchMonitor.Gui/DevicePoller.cs
+++ SwitchMonitor.Gui/DevicePoller.cs
@@ -4,7 +4,6 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.NetworkInformation;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -46,6 +45,7 @@ namespace SwitchMonitor
             using (var db = Database.GetConnection())
             {
                 devices = db.Table<Device>().ToList();
+                lastStatuses.Clear();
             }
         }
 
